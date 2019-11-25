193969fdc772f564da8c208a5ce9133a7e932b4d
sunzid02 <sunzid02@gmail.com>
2019-11-25 21:57:51 +0600

    nested components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import  Ninjas  from "./Ninjas";
+
 
 function App() {
   return (
     <div className="App">
       <h1>  Let the fun begin  </h1>
       <p>  Booyah  </p>
+
+      <Ninjas />
     </div>
   );
 }
